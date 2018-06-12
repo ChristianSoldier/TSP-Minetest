9360adb32823da6f421999e936028604e0560a5a
ChristianSoldier <39861797+ChristianSoldier@users.noreply.github.com>
2018-06-12 12:41:49 -0400

    Add files via upload

---
+++ worldmods/sga/init.lua
@@ -0,0 +1,545 @@
+minetest.register_node("sga:a", {
+	description = ("SGA A"),
+	tiles = {"sga_a.png"},
+	inventory_image = "sga_a.png",
+	wield_image = "sga_a.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:b", {
+	description = ("SGA B"),
+	tiles = {"sga_b.png"},
+	inventory_image = "sga_b.png",
+	wield_image = "sga_b.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:c", {
+	description = ("SGA C"),
+	tiles = {"sga_c.png"},
+	inventory_image = "sga_c.png",
+	wield_image = "sga_c.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:d", {
+	description = ("SGA D"),
+	tiles = {"sga_d.png"},
+	inventory_image = "sga_d.png",
+	wield_image = "sga_d.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:e", {
+	description = ("SGA E"),
+	tiles = {"sga_e.png"},
+	inventory_image = "sga_e.png",
+	wield_image = "sga_e.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:f", {
+	description = ("SGA F"),
+	tiles = {"sga_f.png"},
+	inventory_image = "sga_f.png",
+	wield_image = "sga_f.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:g", {
+	description = ("SGA G"),
+	tiles = {"sga_g.png"},
+	inventory_image = "sga_g.png",
+	wield_image = "sga_g.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:h", {
+	description = ("SGA H"),
+	tiles = {"sga_h.png"},
+	inventory_image = "sga_h.png",
+	wield_image = "sga_h.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:i", {
+	description = ("SGA I"),
+	tiles = {"sga_i.png"},
+	inventory_image = "sga_i.png",
+	wield_image = "sga_i.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:j", {
+	description = ("SGA J"),
+	tiles = {"sga_j.png"},
+	inventory_image = "sga_j.png",
+	wield_image = "sga_j.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:k", {
+	description = ("SGA K"),
+	tiles = {"sga_k.png"},
+	inventory_image = "sga_k.png",
+	wield_image = "sga_k.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:l", {
+	description = ("SGA L"),
+	tiles = {"sga_l.png"},
+	inventory_image = "sga_l.png",
+	wield_image = "sga_l.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:m", {
+	description = ("SGA M"),
+	tiles = {"sga_m.png"},
+	inventory_image = "sga_m.png",
+	wield_image = "sga_m.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:n", {
+	description = ("SGA N"),
+	tiles = {"sga_n.png"},
+	inventory_image = "sga_n.png",
+	wield_image = "sga_n.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:o", {
+	description = ("SGA O"),
+	tiles = {"sga_o.png"},
+	inventory_image = "sga_o.png",
+	wield_image = "sga_o.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:p", {
+	description = ("SGA P"),
+	tiles = {"sga_p.png"},
+	inventory_image = "sga_p.png",
+	wield_image = "sga_p.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:q", {
+	description = ("SGA Q"),
+	tiles = {"sga_q.png"},
+	inventory_image = "sga_q.png",
+	wield_image = "sga_q.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:r", {
+	description = ("SGA R"),
+	tiles = {"sga_r.png"},
+	inventory_image = "sga_r.png",
+	wield_image = "sga_r.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:s", {
+	description = ("SGA S"),
+	tiles = {"sga_s.png"},
+	inventory_image = "sga_s.png",
+	wield_image = "sga_s.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:t", {
+	description = ("SGA T"),
+	tiles = {"sga_t.png"},
+	inventory_image = "sga_t.png",
+	wield_image = "sga_t.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:u", {
+	description = ("SGA U"),
+	tiles = {"sga_u.png"},
+	inventory_image = "sga_u.png",
+	wield_image = "sga_u.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:v", {
+	description = ("SGA V"),
+	tiles = {"sga_v.png"},
+	inventory_image = "sga_v.png",
+	wield_image = "sga_v.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:w", {
+	description = ("SGA W"),
+	tiles = {"sga_w.png"},
+	inventory_image = "sga_w.png",
+	wield_image = "sga_w.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:x", {
+	description = ("SGA X"),
+	tiles = {"sga_x.png"},
+	inventory_image = "sga_x.png",
+	wield_image = "sga_x.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:y", {
+	description = ("SGA Y"),
+	tiles = {"sga_y.png"},
+	inventory_image = "sga_y.png",
+	wield_image = "sga_y.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
+
+minetest.register_node("sga:z", {
+	description = ("SGA Z"),
+	tiles = {"sga_z.png"},
+	inventory_image = "sga_z.png",
+	wield_image = "sga_z.png",
+	groups = {dig_immediate = 2, unbreakable = 1},
+	paramtype = 'light',
+	paramtype2 = "wallmounted",
+	legacy_wallmounted = true,
+	light_source = 14,
+	drawtype = "nodebox",
+	node_box = {
+		type = "wallmounted",
+		wall_top    = {-0.375, 0.4375, -0.5, 0.375, 0.5, 0.5},
+		wall_bottom = {-0.375, -0.5, -0.5, 0.375, -0.4375, 0.5},
+		wall_side   = {-0.5, -0.5, -0.375, -0.4375, 0.5, 0.375},
+	},
+	selection_box = {type = "wallmounted"},
+
+})
